9a49972a10ada24a575a4bb42cc802324a067cd9
Matija Dabic <matijadabic@hotmail.com>
2020-03-29 16:14:06 -0400

    Sunflower app model

---
+++ app/src/main/java/com/example/mona/PagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.mona
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.mona.fragment.CollectionFragment
+import com.example.mona.fragment.ListFragment
+import com.example.mona.fragment.MapFragment
+import com.example.mona.fragment.OeuvreJourFragment
+
+
+const val ODJ_PAGE_INDEX = 0
+const val MAP_PAGE_INDEX = 1
+const val LIST_PAGE_INDEX = 2
+const val COLLECTION_PAGE_INDEX = 3
+
+class PagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    /**
+     * Mapping of the ViewPager page indexes to their respective Fragments
+     */
+    private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
+        ODJ_PAGE_INDEX to { OeuvreJourFragment() },
+        MAP_PAGE_INDEX to { MapFragment() },
+        LIST_PAGE_INDEX to { ListFragment() },
+        COLLECTION_PAGE_INDEX to { CollectionFragment() }
+    )
+
+    override fun getItemCount() = tabFragmentsCreators.size
+
+    override fun createFragment(position: Int): Fragment {
+        return tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
+    }
+
+}
---
+++ app/src/main/java/com/example/mona/fragment/HomeViewPagerFragment.kt
@@ -0,0 +1,71 @@
+package com.example.mona.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.example.mona.*
+import com.example.mona.databinding.*
+import com.google.android.material.tabs.TabLayout
+
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class HomeViewPagerFragment(): Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = FragmentViewPagerBinding.inflate(inflater, container, false)
+
+        val tabLayout = binding.tabs
+        
+        //Colors on tab icons
+        tabLayout.tabIconTint = null
+
+        val viewPager = binding.viewPager
+
+        viewPager.isUserInputEnabled = false
+
+        viewPager.adapter = PagerAdapter(this)
+
+        // Set the icon and text for each tab
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.setIcon(getTabIcon(position))
+            tab.text = getTabTitle(position)
+        }.attach()
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
+        return binding.root
+    }
+
+    private fun getTabIcon(position: Int): Int {
+        return when (position) {
+            ODJ_PAGE_INDEX -> R.drawable.odj
+            MAP_PAGE_INDEX -> R.drawable.map
+            LIST_PAGE_INDEX -> R.drawable.list
+            COLLECTION_PAGE_INDEX -> R.drawable.collection
+            else -> throw IndexOutOfBoundsException()
+        }
+    }
+
+    private fun getTabTitle(position: Int): String? {
+        return when (position) {
+            ODJ_PAGE_INDEX -> getString(R.string.ODJ)
+            MAP_PAGE_INDEX -> getString(R.string.Map)
+            LIST_PAGE_INDEX -> getString(R.string.List)
+            COLLECTION_PAGE_INDEX -> getString(R.string.Collection)
+            else -> null
+        }
+    }
+
+}
